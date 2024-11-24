41a899389b97c264bed9e034885858f5af4d7de9
JulianK123 <julian.kainz@htlpinkafeld.at>
2024-11-24 19:04:35 +0100

    added output for chat output and input from user

---
+++ src/shared/components/atoms/output-field/output-field.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output-field',
+  standalone: true,
+  imports: [],
+  templateUrl: './output-field.component.html',
+  styleUrl: './output-field.component.scss'
+})
+export class OutputFieldComponent {
+
+}
--- src/shared/components/pages/wep-page/wep-page.component.ts
+++ src/shared/components/pages/wep-page/wep-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputFieldComponent } from '../../atoms/input-field/input-field.component';
+import { OutputFieldComponent } from '../../atoms/output-field/output-field.component';
 
 @Component({
   selector: 'app-wep-page',
   standalone: true,
-  imports: [InputFieldComponent],
+  imports: [OutputFieldComponent],
   templateUrl: './wep-page.component.html',
   styleUrl: './wep-page.component.scss',
 })
