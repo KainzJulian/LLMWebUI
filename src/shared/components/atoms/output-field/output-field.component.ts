14e8438b00384923e988d44d677d9d9149149db6
JulianK123 <julian.kainz@htlpinkafeld.at>
2024-11-30 07:33:29 +0100

    finished output field

--- src/shared/components/atoms/output-field/output-field.component.ts
+++ src/shared/components/atoms/output-field/output-field.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-output-field',
   standalone: true,
   imports: [],
   templateUrl: './output-field.component.html',
-  styleUrl: './output-field.component.scss'
+  styleUrl: './output-field.component.scss',
 })
 export class OutputFieldComponent {
-
+  @Input() text: string = '';
+  @Input() textStyle: 'humanText' | 'aiText' = 'aiText';
 }
