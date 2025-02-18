cf9695e117e473980c4b8e6afc7b25d7b0310d15
JulianK123 <julian.kainz@htlpinkafeld.at>
2025-02-18 20:17:32 +0100

    rename chat #14

--- frontend/src/shared/components/atoms/list-button/list-button.ts
+++ frontend/src/shared/components/atoms/list-button/list-button.ts
@@ -1,9 +1,12 @@
 import {
   Component,
+  ElementRef,
   EventEmitter,
   HostBinding,
+  HostListener,
   Input,
   Output,
+  ViewChild,
 } from '@angular/core';
 import { BaseButton } from '../base-button/base-button';
 import { CommonModule } from '@angular/common';
@@ -11,7 +14,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'list-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BaseButton],
   templateUrl: './list-button.html',
   styleUrl: './list-button.scss',
 })
@@ -20,8 +23,30 @@ export class ListButton {
   @Input() iconName: string = '';
   @Input() isSelected: boolean = false;
 
+  @Input() openOptionsOnIconClick = false;
+
+  @ViewChild('options') options!: ElementRef;
+
   isHovering: boolean = false;
+
+  optionsClass: 'closed' | 'open' = 'closed';
 
   @Output() onClickButton = new EventEmitter<void>();
   @Output() onClickIcon = new EventEmitter<void>();
+
+  clickIcon() {
+    // if (this.openOptionsOnIconClick) {
+    //   this.optionsClass = this.optionsClass == 'closed' ? 'open' : 'closed';
+    // } else {
+    //   this.onClickIcon.emit();
+    // }
+  }
+
+  // @HostListener('document:click', ['$event'])
+  // onClickOutside(event: Event) {
+  //   console.log(this.options);
+
+  //   if (!this.options.nativeElement.contains(event.target))
+  //     this.optionsClass = 'closed';
+  // }
 }
