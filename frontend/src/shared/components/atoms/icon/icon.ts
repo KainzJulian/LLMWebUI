f74904e4d93c514f9953b6fdef904e3dadbc65ae
JulianK123 <julian.kainz@htlpinkafeld.at>
2025-02-22 21:41:31 +0100

    feat: icon component for easier icon use

---
+++ frontend/src/shared/components/atoms/icon/icon.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'icon, base-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon.html',
+  styleUrl: './icon.scss',
+})
+export class Icon {
+  @Input() iconName!: string;
+}
