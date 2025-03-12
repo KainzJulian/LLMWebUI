69ba2663fbb85763bf13bac225930196925ed540
JulianK123 <julian.kainz@htlpinkafeld.at>
2025-03-12 21:36:57 +0100

    feat: added base service and chat service to manage ai models for different task

---
+++ backend/services/baseService.py
@@ -0,0 +1,31 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, TextStreamer
+import torch
+import os
+
+
+class BaseService:
+    def __init__(self, model: str, task: str, device: str = "cpu"):
+
+        self.task = task
+        self.model = model
+        self.generator = None
+        self.streamer = None
+        self.device = device
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
+
+    def generate(self, input) -> str:
+        self.generator = pipeline(
+            self.task, tokenizer=self.tokenizer, model=self.model, device=self.device
+        )
+
+        self.streamer = TextStreamer(self.tokenizer)
+
+        output = self.generator(
+            input, do_sample=False, return_full_text=True, streamer=self.streamer
+        )
+
+        return output
+
+    def getStreamer(self):
+        return self.streamer
---
+++ backend/services/chatService.py
@@ -0,0 +1,7 @@
+from baseService import BaseService
+
+
+class ChatService(BaseService):
+
+    def __init__(self):
+        super().__init__()
