047a23d53f9d244b60f879558615711e5f26a10e
JulianK123 <julian.kainz@htlpinkafeld.at>
2025-03-14 17:58:40 +0100

    feat: added test files and added .env file variables into config and database connection

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,4 +1,8 @@
 from .aiModelRoute import aiModelRouter
 from .chatRoute import chatRouter
+<<<<<<< HEAD
 
 __all__ = ["aiModelRouter", "chatRouter"]
+=======
+from .optionsRoute import optionsRouter
+>>>>>>> 7430eaf (feat: added test files and added .env file variables into config and database connection)
