dbba4fa2086e07172eb8328866a0de327247aaf6
JulianK123 <juliankainz8@gmail.com>
2025-03-18 14:16:47 +0100

    fix: resolve editor change removed

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,8 +1,4 @@
 from .aiModelRoute import aiModelRouter
 from .chatRoute import chatRouter
-<<<<<<< HEAD
 
 __all__ = ["aiModelRouter", "chatRouter"]
-=======
-from .optionsRoute import optionsRouter
->>>>>>> 7430eaf (feat: added test files and added .env file variables into config and database connection)
